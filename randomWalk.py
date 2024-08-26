b28cf172248c0cb18ab77a4d8b79fe346dd990fe
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-26 16:07:59 +0600

    added rgb colors

--- randomWalk.py
+++ randomWalk.py
@@ -1,29 +1,38 @@
-from turtle import Turtle, Screen
+import turtle as t
 from random import randint
 
-turtle_obj = Turtle()
-screen_obj = Screen()
+turtle_obj = t.Turtle()
+screen_obj = t.Screen()
 turtle_obj.shape("circle")
 turtle_obj.shapesize(0.5, 0.5, 0.5)
 turtle_obj.pensize(10)
+t.colormode(255)
 
-colors = [
-    "deep pink",
-    "dark violet",
-    "indigo",
-    "deep sky blue",
-    "steel blue",
-    "lime",
-    "maroon",
-]
+# colors = [
+#     "deep pink",
+#     "dark violet",
+#     "indigo",
+#     "deep sky blue",
+#     "steel blue",
+#     "lime",
+#     "maroon",
+# ]
+
+def randomColor ():
+    redLevel = randint (0, 255)
+    greenLevel = randint (0, 255)
+    blueLevel = randint (0, 255)
+    rgb  = (redLevel, greenLevel, blueLevel) # this is a tuple
+    return rgb
+
 angles = [0, 90, 180, 270]
 
 for i in range (300):
     # choiceDir = randint(0, 3)
     choiceAngle = angles[randint(0, 3)]
-    turtle_obj.color(colors[randint(0,6)])
+    turtle_obj.color(randomColor())
     turtle_obj.speed("fastest")
-    
+
     # if choiceDir == 0:
     #     turtle_obj.forward(20)
     # elif choiceDir == 1:
