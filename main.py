1c7189ec60fd015b3f9d0dbf26809c98933c8923
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-26 15:39:09 +0600

    cleaned main file

--- main.py
+++ main.py
@@ -3,22 +3,12 @@ from random import randint
 
 turtle_obj = Turtle()
 screen_obj = Screen()
-turtle_obj.shape("circle")
+turtle_obj.shape("square")
 
-# for i in range (15):
-#     turtle_obj.forward(10)
-#     turtle_obj.penup()
-#     turtle_obj.forward(10)
-#     turtle_obj.pendown()
-
-colors = [
-    "deep pink",
-    "dark violet",
-    "indigo",
-    "deep sky blue",
-    "steel blue",
-    "lime",
-    "maroon",
-]
+for i in range (15): ## dotted line
+    turtle_obj.forward(10)
+    turtle_obj.penup()
+    turtle_obj.forward(10)
+    turtle_obj.pendown()
 
 screen_obj.exitonclick()
