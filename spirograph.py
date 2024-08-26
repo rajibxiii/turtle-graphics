c9257a294d5df70178d1eae1871d2fcfe5629b9d
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-26 16:46:09 +0600

    completed the spirograph project

--- spirograph.py
+++ spirograph.py
@@ -5,22 +5,26 @@ turtle_obj = t.Turtle()
 screen_obj = t.Screen()
 t.colormode(255)
 
-def randomColor ():
-    redLevel = randint (0, 255)
-    greenLevel = randint (0, 255)
-    blueLevel = randint (0, 255)
-    rgb  = (redLevel, greenLevel, blueLevel) # this is a tuple
+
+def randomColor():
+    redLevel = randint(0, 255)
+    greenLevel = randint(0, 255)
+    blueLevel = randint(0, 255)
+    rgb = (redLevel, greenLevel, blueLevel)  # this is a tuple
     return rgb
 
-# for i in range (300):
-#     # choiceDir = randint(0, 3)
-#     choiceAngle = angles[randint(0, 3)]
-#     turtle_obj.color(randomColor())
-#     turtle_obj.speed("fastest")
-
-#     turtle_obj.forward(20)
-#     turtle_obj.setheading(angles[randint(0, 3)])
 
 turtle_obj.speed("fastest")
-turtle_obj.color(randomColor())
-turtle_obj.circle(50)
+
+
+def draw_spirogrraph(gapSize):
+    for i in range(int(360 / gapSize)):
+        headedNow = turtle_obj.heading()
+        turtle_obj.setheading(headedNow + gapSize)
+        turtle_obj.color(randomColor())
+        turtle_obj.circle(50)
+
+
+draw_spirogrraph(5)
+
+screen_obj.exitonclick()
